20eeabb85462198d66db2ab7dad516d1c86b6afb
stani1940 <stanislav1940@abv.bg>
2020-04-13 17:54:24 +0300

    ADD structure (index,header and footer)

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="includes/login.css">
+
+    <title><?php echo $page_title ?></title>
+
+</head>
